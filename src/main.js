b98963508eb1fc2312f5f302522494701eeb3514
Bron N Thulke <bron@awd.angelwebdesigns.com.au>
2024-06-30 19:40:39 +1000

    Change to use env variable

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ Vue.config.productionTip = false
 
 const appInsights = new ApplicationInsights({ 
   config: {
-    connectionString: "InstrumentationKey=af9a5c48-6afb-4b37-ae5b-bec83723a2dd;IngestionEndpoint=https://westus2-0.in.applicationinsights.azure.com/;LiveEndpoint=https://westus2.livediagnostics.monitor.azure.com/;ApplicationId=0bbe6ded-07fa-497a-b09a-90c2eda99659"
+    connectionString: "process.env.VUE_APP_APPINSIGHTS_CONNECTION_KEY"
     /* …Other Configuration Options… */
   }
 });
