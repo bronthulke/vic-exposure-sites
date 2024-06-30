e79df7302ba02bd35c5c85660a4857c960461e5e
Bron N Thulke <bron@awd.angelwebdesigns.com.au>
2024-06-30 19:37:59 +1000

    Change app insights

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ Vue.config.productionTip = false
 
 const appInsights = new ApplicationInsights({ 
   config: {
-    connectionString: process.env.APPINSIGHTS_INSTRUMENTATIONKEY
+    connectionString: "InstrumentationKey=af9a5c48-6afb-4b37-ae5b-bec83723a2dd;IngestionEndpoint=https://westus2-0.in.applicationinsights.azure.com/;LiveEndpoint=https://westus2.livediagnostics.monitor.azure.com/;ApplicationId=0bbe6ded-07fa-497a-b09a-90c2eda99659"
     /* …Other Configuration Options… */
   }
 });
