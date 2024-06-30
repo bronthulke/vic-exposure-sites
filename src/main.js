7d59a9600d155ca0f71d531069f5e4143c26fe57
Bron N Thulke <bron@awd.angelwebdesigns.com.au>
2024-06-30 21:09:26 +1000

    Add App Insights for web

--- src/main.js
+++ src/main.js
@@ -6,6 +6,17 @@ import '@/styles.css'
 
 Vue.config.productionTip = false
 
+const appInsights = new ApplicationInsights({ 
+  config: {
+    connectionString: "process.env.VUE_APP_APPINSIGHTS_CONNECTION_KEY"
+    /* …Other Configuration Options… */
+  }
+});
+  
+appInsights.loadAppInsights();
+
+appInsights.trackPageView();
+  
 new Vue({
   render: h => h(App),
 }).$mount('#app')
